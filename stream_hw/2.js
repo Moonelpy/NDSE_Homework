d8c634beade76020d540df7cb6cf79a5e363f880
Ivan Zamorin <Ivan.Zamorin@x5.ru>
2024-05-30 10:13:36 +0300

    Правки

--- stream_hw/2.js
+++ stream_hw/2.js
@@ -1,38 +1,37 @@
 const fs = require('fs');
 const path = require('path');
 
-const args = process.argv.slice(2);
+const logFilePath = process.argv[2];
+const logPath = path.join(__dirname, logFilePath);
 
-const logFilePath = path.resolve(args[0]);
-
-function analyzeLogs(logFilePath) {
-  fs.readFile(logFilePath, 'utf8', (err, data) => {
-    if (err) {
-      console.log('Ошибка чтения файла:', err);
+fs.readFile(logPath, 'utf8', (err, data) => {
+  if (err) {
+    console.log('Ошибка чтения файла:', err);
     return;
+  }
+  let wins = 0;
+  let losses = 0;
+
+  // Разбиваем лог на массив строк по \n
+  const lines = data.trim().split('\n');
+  // Считаем кол-во линий(кол-во игр)
+  const totalGames = lines.length;
+
+  // По линиям считаем кол-во Побед и Проигрышей в зависимости от наличия слова "Победа" или "Проигрыш"
+  lines.forEach((line) => {
+    if (line.includes('Победа')) {
+      wins++;
+    } else if (line.includes('Проигрыш')) {
+      losses++;
     }
-    let wins = 0;
-    let losses = 0;
-
-    const lines = data.trim().split('\n');
-    const totalGames = lines.length;
-    
-    //Парсим линии в зависимости от наличия слова "Победа" или "Проигрыш"
-    lines.forEach(line => {
-      if (line.includes('Победа')) {
-        wins++;
-      } else if (line.includes('Проигрыш')) {
-        losses++;
-      }
-    });
-
-    // Считаем процен побед
-    const winPercentage = (wins / totalGames) * 100;
-
-    console.log(`Общее количество партий: ${totalGames}`);
-    console.log(`Победа: ${wins} / Поражение: ${losses}`);
-    console.log(`Процент побед: ${winPercentage.toFixed(2)}%`);
   });
-}
-// Для корректного запуска нужно передать имя файла, в данном случае log.txt
-analyzeLogs(logFilePath);
+
+  // Считаем процен побед
+  const winPercentage = (wins / totalGames) * 100;
+
+  console.log(`Общее количество партий: ${totalGames}`);
+  console.log(`Победа: ${wins} / Поражение: ${losses}`);
+  console.log(`Процент побед: ${winPercentage.toFixed(2)}%`);
+});
+
+/*Программа принимает в качестве аргумента log.txt */
