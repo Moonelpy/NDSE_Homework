57dd112cbb8e2777a3dae6ca5553e862af90b4c4
Иван Заморин <Ivan.Zamorin@x5.ru>
2024-06-20 22:59:34 +0300

    разнес файлы и залил образ

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 require("dotenv").config();
 const express = require('express');
 const app = express();
-
-const authRouter = require('./routes/api/auth');
-const apiBooksRouter = require('./routes/api/books');
-const modBooksRouter = require('./routes/mod/books');
-const indexRouter = require('./routes/index')
-const errorMiddleware = require('./middleware/error/404')
+const path = require('path');
+const authRouter = require('./src/routes/api/auth');
+const apiBooksRouter = require('./src/routes/api/books');
+const modBooksRouter = require('./src/routes/mod/books');
+const indexRouter = require('./src/routes/index')
+const errorMiddleware = require('./src/middleware/error/404')
 
 app.use(express.static("database/fileBooks"));
 app.use(express.urlencoded({ extended: true }));
@@ -18,6 +18,7 @@ app.use('/api/user', authRouter);
 app.use('/api/books', apiBooksRouter); 
 app.use('/mod/books', modBooksRouter);
 app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, 'src', 'views'));
 app.use(errorMiddleware);
 
 // Тут Number иначе не запускает и пишет что порт занят
