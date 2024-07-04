75045d39302dfc96168abcb8f7bd256e2eaeb09b
Иван Заморин <Ivan.Zamorin@x5.ru>
2024-07-04 23:02:12 +0300

    Сокеты

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
 require("dotenv").config();
+require('./src/config/passport')
 const express = require('express');
 const path = require('path');
 const mongoose = require("mongoose");
+const http = require('http')
+const socketIO = require('socket.io')
+
 const userRouter = require('./src/routes/mod/user');
 const apiBooksRouter = require('./src/routes/api/books');
 const modBooksRouter = require('./src/routes/mod/books');
 const indexRouter = require('./src/routes/index')
 const errorMiddleware = require('./src/middleware/error/404')
+
 const session = require('express-session')
 const passport = require('passport')
-require('./src/config/passport')
+  
 
 const app = express();
+const server = http.createServer(app)
+const io = socketIO(server)
 
 app.use(session({ secret: 'SECRET'}));
 app.use(passport.initialize())
@@ -30,16 +37,47 @@ app.set("view engine", "ejs");
 app.set('views', path.join(__dirname, 'src', 'views'));
 app.use(errorMiddleware);
 
+io.on('connection', (socket) => {
+    const {id} = socket;
+    console.log(`Socket connected: ${id}`);
+
+    const {roomName} = socket.handshake.query;
+    console.log(`Socket roomName: ${roomName}`);
+    socket.join(roomName);
+
+    // Обработка события отправки комментария в комнату
+    socket.on('message-to-room', (msg) => {
+        msg.type = `room: ${roomName}`;
+        socket.to(roomName).emit('message-to-room', msg);
+        socket.emit('message-to-room', msg);
+    });
+
+    // Обработка события отправки комментария
+    socket.on('comment message', (msg) => {
+        msg.type = `room: ${roomName}`;
+        io.emit('comment message', msg.username + ': ' + msg.text)
+    })
+    
+    // Отключение клиента 
+    socket.on('disconnect', () => {
+        console.log(`Socket disconnected: ${id}`);
+    });
+});
+
+
+
 const PORT = Number(process.env.PORT) || 3000;
 const UrlDB = process.env.MONGO_URL || 'mongodb://root:password@mongo:27017/'
 async function start(PORT, UrlDB) {
     try {
-        await mongoose.connect(UrlDB).then(() => console.log("Mongo connected!"));
-        app.listen(PORT)
-        console.log(`Сервер запущен на порту ${PORT}`)
+        await mongoose.connect(UrlDB);
+        console.log("Mongo connected!");
+        server.listen(PORT, () => {
+            console.log(`Сервер запущен на порту ${PORT}`);
+        });
     } catch (e) {
         console.log(e);
     }
 }
 
-start(PORT, UrlDB)
+start(PORT, UrlDB);
